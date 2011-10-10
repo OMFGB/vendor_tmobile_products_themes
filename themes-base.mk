55ad11791165ee69190581f44b1b147f6774e77a
r2doesinc <r2doesinc@gmail.com>
2011-10-10 09:03:17 -0400

    Dont include androidian and cyanbread.

--- themes-base.mk
+++ themes-base.mk
@@ -1,9 +1,7 @@
 PRODUCT_PACKAGES += \
 	ThemeManager \
 	ThemeChooser \
-	com.tmobile.themes \
-	Androidian \
-	Cyanbread
+	com.tmobile.themes 
 
 PRODUCT_COPY_FILES := \
 	frameworks/base/data/etc/com.tmobile.software.themes.xml:/system/etc/permissions/com.tmobile.software.themes.xml
